654273bcfda2e76d72e315ad4a25f5432ad6328f
Luis Guimaraes <l.guimaraes@amtrans.com.br>
2024-11-29 18:47:34 -0300

    tentativa ajuste github

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ProjetoExtensaoProgWeb/',
   plugins: [
     vue({
       template: { transformAssetUrls }
